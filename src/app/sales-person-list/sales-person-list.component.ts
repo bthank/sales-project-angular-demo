9bf6befe7683c834339d8e29e587ecc4b8e60fbb
bthank <bthankachan@yahoo.com>
2021-07-19 19:46:55 -0400

    Added changes to display sales person data on html page using Angular

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+
+  // create an array of sales person objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("John","Layne","john.layne@gmail.com",75000),
+    new SalesPerson("Amy","Taylow","amy.taylor@aol.com",90000),
+    new SalesPerson("Myra","Banks","myra.banks@gmail.com",80000),
+    new SalesPerson("Peter","Stadler","peter.stadler@aol.com",40000),
+    new SalesPerson("Kara","Edwards","kara.edwards@gmail.com",55000)
+
+
+  ];
+
+  
+
 
   constructor() { }
 
